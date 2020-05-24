ace2f3c2395591214365244f2b4875b842470686
venki1995 <venkinellore310@gmail.com>
2020-05-24 17:15:28 +0530

    convergence corrected

--- LinearRegression.py
+++ LinearRegression.py
@@ -20,28 +20,24 @@ class LinearRegression:
     def fit(self,X,y):
         self.cost = []
         x = X.shape[0]
-        self.w = np.random.random((X.shape[1]+1,1))
+        random_state = np.random.RandomState(45)
+        self.w = random_state.random((X.shape[1]+1,1))
         for i in range(self.n_iterations):
             residuals = y - self.predict(X)
             self.w[1:] = self.w[1:] + self.eta*(np.dot(X.T,residuals)/x) #loss function = 1/2m(y-ypred)^2
             self.w[0] = self.w[0] + self.eta*(np.sum(residuals)/x)
             self.cost.append(np.sum(residuals**2)/2*x)
-            if np.abs(np.sum(residuals)) < 10**-3: #convergence criteria
-                self.convergence = i
-                break
-            else:
-                self.convergence = i
         return self
     
     def plot_cost(self):
         fig,ax1 = plt.subplots(dpi=250)
-        ax1.plot(np.arange(self.convergence+1),self.cost,label='cost',marker='.')
+        ax1.plot(np.arange(self.n_iterations),self.cost,label='cost',marker='.')
         ax1.set(title='Cost vs Iterations',ylabel='Cost',xlabel='n_iterations')
         ax1.legend()
     
 x = np.arange(0,10,0.5).reshape(-1,1)       #input x signals
-random_state = np.random.RandomState(42)
-y = 0.5*x + 3*random_state.random(x.shape)  #input y signals
+seed = np.random.RandomState(42)
+y = 0.5*x + 3*seed.random(x.shape)  #input y signals
 
 lm = LinearRegression()
 lm.fit(x,y)
